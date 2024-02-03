5ee83c19ecdf910d0a4cc5ebf84faaf2b5344961
djoudi <djoudi@MBP-de-djoudi.lan>
2024-02-03 13:29:00 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{to-do-list}',
+ version='1.0',
+ author='djd951',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
